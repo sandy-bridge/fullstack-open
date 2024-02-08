c54e61863b37f1428dd2a32792322ea40fcfcaf0
Mihkel Kiil <kiil.mihkel@gmail.com>
2024-02-08 13:35:08 +0200

    Remove defaults

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -5,25 +5,16 @@ import './App.css'
 
 const Hello = (props) => {
   return (
-    <div>
+    <>
       <p>Hello {props.name}</p>
-    </div>
+    </>
   )
 }
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <Hello name="Daisy"/>
     </>
