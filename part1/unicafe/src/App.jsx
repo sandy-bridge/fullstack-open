4ba4619c1c2713a9111c96e89f8589a3f7d58922
Mihkel Kiil <kiil.mihkel@gmail.com>
2024-02-12 15:24:12 +0200

    Create structure for percentage and average

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,6 +11,8 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [percentage, setPercentage] = useState(0)
 
   return (
     <div>
@@ -26,6 +28,12 @@ const App = () => {
       </p>
       <p>
         Bad: {bad}
+      </p>
+      <p>
+        Average: {average}
+      </p>
+      <p>
+        Percentage: {percentage}
       </p>
     </div>
   )
