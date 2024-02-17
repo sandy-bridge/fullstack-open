37d81d49243a08cebe3525424f58052a92800bb1
Mihkel Kiil <kiil.mihkel@gmail.com>
2024-02-17 21:28:48 +0200

    Fix exercises

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -17,9 +17,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercises1={props.exercises1} />
-      <Part part={props.part2} exercises1={props.exercises2} />
-      <Part part={props.part3} exercises1={props.exercises3} />
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   )
 }
