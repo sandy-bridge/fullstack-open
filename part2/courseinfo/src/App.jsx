9c8e4fb140c5afd8ae7713215e9542e097632043
Mihkel Kiil <kiil.mihkel@gmail.com>
2024-02-17 21:59:20 +0200

    2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,43 +1,4 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
-
-const Total = ({ parts }) => {
-  const sum = parts.reduce(
-    (accumulator, current) => accumulator + current.exercises,
-    0);
-  return (
-    <div>
-      <strong>The sum of exercises is {sum}</strong>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => 
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      )}
-    </div>
-  )
-}
-
-const Course = ({ course }) => (
-  <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-  </div>
-)
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
