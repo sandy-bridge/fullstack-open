1d4f4d9204cdb3dca18388a19421b47aa65f52ff
Mihkel Kiil <kiil.mihkel@gmail.com>
2024-03-02 18:55:35 +0000

    Remove unused import

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
-import { Axios } from 'axios'
 
 const App = () => {
   const [persons, setPersons] = useState([
